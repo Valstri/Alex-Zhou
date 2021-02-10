edc6b9b31a24b95707eeef77a6eec161654e5f8d
Alex Zhou <zhoualex557@gmail.com>
2021-02-09 17:40:05 -0800

    T

---
+++ A/src/A.java
@@ -0,0 +1,16 @@
+public class A
+{
+    public static void main(String[] args) {
+        int roomnumber;
+        double e;
+        roomnumber = 113;
+        e = 2.71828;
+        String Subject = "Computer science";
+        String Room = "There is room #";
+        String NL ="e is close to";
+        String CS = "I am learning a bit about";
+        System.out.println( Room + roomnumber );
+        System.out.println( NL + " " + e );
+        System.out.println( CS + " " + Subject );
+    }
+}
---
+++ A/src/SCEDULE.java
@@ -0,0 +1,33 @@
+public class SCEDULE
+{
+    public static void main(String[] args) {
+        String En = "English composition 11";
+        String Math1 = "Pre-calculus 12";
+        String Math2 = "Calculus 12";
+        String Cs = "Computer programming";
+        String Ss = "Social study";
+        String Ls = "Life science 11";
+        String Phy = "Physic 12";
+        String ELD = "English learning development";
+        String TEn = "Mr.Tryssenaar";
+        String TMath1 = "Mr.Griffiths";
+        String TMath2 = "VLN";
+        String TCs = "Mr.Zaremba";
+        String TSs = "Quon";
+        String TLs = "Ms.Annas";
+        String TPhy = "Mr.Hemlen";
+        String TELD = "Ms.soo";
+        System.out.println( "+---------------------------------------+" );
+        System.out.println( "|1|" + En + " | " + TEn );
+        System.out.println( "|2|" + Math1+ " | " + TMath1 );
+        System.out.println( "|3|" + Cs + " | " + TCs );
+        System.out.println( "|4|" + Ss + " | " + TCs );
+        System.out.println( "|5|" + Ls + " | " + TLs );
+        System.out.println( "|6|" + Phy + " | " +TPhy );
+        System.out.println( "|7|" + ELD + " | " + TELD );
+        System.out.println( "|8|" + Math2 + " | " + TMath2 );
+        System.out.println( "+---------------------------------------+" );
+
+
+    }
+}

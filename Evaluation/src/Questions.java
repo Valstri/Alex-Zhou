86cfa57526f3a644a2e9f3e193519da4bae0ea56
Alex Zhou <2321466@learn.vsb.bc.ca>
2021-02-10 10:23:10 -0800

    Evaluation Assignment

---
+++ Evaluation/src/Questions.java
@@ -0,0 +1,49 @@
+public class Questions {
+    public static void main(String[] args) {
+        //Question 1
+        int x = 5;
+        int y = 7;
+        System.out.println(((x*4) < (y + 15)) && x < y);
+        System.out.println(20 < (y + 15) && x < y);
+        System.out.println(20 < 22 && x < y);
+        System.out.println(20 < 22 && 5 < 7);
+        System.out.println(true);
+        //Question 2
+        x = 10;
+        y = 20;
+        int z = 30;
+        System.out.println((( x == y) || (x < z)) && (x*2 + y)/5 > z);
+        System.out.println((10 == 20 || x < z) && (x*2 +y)/5 > z);
+        System.out.println(10 == 20|| 10<2 && (x*2 +y)/5 > z);
+        System.out.println((10 == 20 || 10 < 30) && ( 20 + 20 )/5 > 30);
+        System.out.println(false);
+
+        //Question 3
+        boolean a = true;
+        boolean b = false;
+        System.out.println((a || b) && ((a == b) || a));
+        System.out.println((true || false) && ((a == b) || a));
+        System.out.println((true || false) && ((true == false) || true));
+        System.out.println(true);
+
+        //Question 4
+        a = false;
+        b = false;
+        x = 5;
+        y = 10;
+        System.out.println((a == b) && (x * 4) == (y % 6) * 5);
+        System.out.println((true == false) && (x * 4) == (y % 6) * 5);
+        System.out.println((true == false) && 20 == (y % 6) * 5);
+        System.out.println((true == false) && 20 == 4 * 5);
+        System.out.println(false && true);
+        System.out.println(false);
+
+
+        //Question 5
+        System.out.println((Math.abs(-2) == 2) || (Math.sqrt(Math.pow(9,2)) <= 9 && (-4 <= 4)));
+        System.out.println((2 == 2) || Math.sqrt(Math.pow(9,2)) <= 9 && (-4 <= 4));
+        System.out.println((2 == 2) || (9 <= 9) && (-4 <=4));
+        System.out.println(true || true && true);
+        System.out.println(true);
+    }
+}
---
+++ Evaluation/src/T.java
@@ -0,0 +1,6 @@
+public class T
+{
+    public static void main(String[] args) {
+        System.out.println((Math.abs(-2) == 2) || (Math.sqrt(Math.pow(9,2)) <= 9 && (-4 <= 4)));
+    }
+}

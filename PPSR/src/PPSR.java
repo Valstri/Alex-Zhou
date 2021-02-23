e382e835f8e46c9c72483e9d1c60980d89c9f75a
Alex Zhou <2321466@learn.vsb.bc.ca>
2021-02-23 08:50:50 -0800

    PPSR

--- PPSR/src/PPSR.java
+++ PPSR/src/PPSR.java
@@ -3,60 +3,69 @@ import java.util.Scanner;
 public class PPSR {
     public static void main(String[] args) {
         //Random part
-
         int PC;
-        PC = (int) (Math.random() * 3) + 1;
-
+        int PYS = 0; int PCS = 0;
+    //PYS is player score and PCS is computer score.
         String P = "paper";
         String S = "Scissors";
         String R = "Rock";
 
-        //Scanner Part turn string into int by <int PY = Integer.parseInt(player)>;
-
+        //Use while true to repeat the program.
         while (true) {
+            PC = (int) (Math.random() * 3) + 1;
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Paper > Rock, Rock > Scissors, Scissors > Paper");
-            System.out.println("Now choose 1.Paper, 2.Scissors, 3.Rock or 4.Exit (Enter the number only)");
+            System.out.println("\n" + "Paper > Rock, Rock > Scissors, Scissors > Paper");
+            System.out.println("Now choose 1.Paper 2.Scissors 3.Rock 4.Exit (Enter the number only)");
             String player = scanner.next();
             int PY = Integer.parseInt(player);
+            // Stop the program when player enter "4"
+            if(PY == 4){
+                break;
+            }
+            if (PY != 1 && PY != 2 && PY != 3 && PY != 4) {
+                System.out.print("Pick valid option!!");
+            }
+
             // Use if to print "PC" vs "PY"
-            if (PC == 1) {
-                System.out.print("PC: " + P + " " + "vs" + " ");
-            }
-            if (PC == 2) {
-                System.out.print("PC: " + S + " " + "vs" + " ");
-            }
-            if (PC == 3) {
-                System.out.print("PC: " + R + " " + "vs" + " ");
-            }
-            if (PY == 1) {
-                System.out.print("PY: " + P);
-            }
-            if (PY == 2) {
-                System.out.print("PY: " + S);
-            }
-            if (PY == 3) {
-                System.out.print("PY: " + R);
-            }
+            if ((PY == 1) || (PY == 2) || (PY == 3)) {
+                if (PC == 1) {
+                    System.out.print("PC: " + P + " " + "vs" + " ");
+                }
+                if (PC == 2) {
+                    System.out.print("PC: " + S + " " + "vs" + " ");
+                }
+                if (PC == 3) {
+                    System.out.print("PC: " + R + " " + "vs" + " ");
+                }
+                if (PY == 1) {
+                    System.out.print("PY: " + P);
+                }
+                if (PY == 2) {
+                    System.out.print("PY: " + S);
+                }
+                if (PY == 3) {
+                    System.out.print("PY: " + R);
+                }
 
+                //Use if to print out the result
+                boolean PYW = (PY == 1 && PC == 3) || (PY == 2 && PC == 1) || (PY == 3 && PC == 2);
+                boolean PCW = (PY == 1 && PC == 2) || (PY == 2 && PC == 3) || (PY == 3 && PC == 1);
+                //Use if to add the score
+                if (PYW) {
+                    System.out.print("\n" + " You won! >w<");
+                    PYS++;
+                }
+                if (PCW) {
+                    System.out.print("\n" + "You lose T_T");
+                    PCS++;
+                }
+                if (PY == PC) {
+                    System.out.print("\n" +" Draw");
+                }
 
-            //Use if to print out the result
-            boolean PYW = (PY == 1 && PC == 3) || (PY == 2 && PC == 1) || (PY == 3 && PC == 2);
-            boolean PYL = (PY == 1 && PC == 2) || (PY == 2 && PC == 3) || (PY == 3 && PC == 1);
-            boolean DRAW = PY == PC;
-
-            if (PYW) {
-                System.out.print(" You won! >w<");
+                System.out.println("\n" + "PlayerScore: " + PYS + " " + "ComputerScore " + PCS);
             }
-            if (PYL) {
-                System.out.print(" You lose T_T");
-            }
-            if (DRAW) {
-                System.out.print(" Draw");
-            }
-            //While Loop, Score board Player score = PLS  ||  Computer score = PCS
         }
+        System.out.print("Thanks for playing");
     }
-    }
-
-
+}
